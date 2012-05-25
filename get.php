b04110999a85678b46667ff652e575fc34d2cd3c
Kieran <kieranajp@me.com>
2012-05-25 02:45:50 +0100

    Made it work etc., really do need to sleep.

--- get.php
+++ get.php
@@ -1,8 +1,7 @@
 <?php
-  if (isset($_POST['uname'])) {
-    $url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&screen_name=".$_REQUEST['user'];
-    echo file_get_contents($url);
+	if (isset($_GET['since') && $_GET['since'] != 0) {
+		$url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&count=200&screen_name=".$_GET['user']."&since_id=".$_GET['since'];
+	} else {
+  	$url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&count=200&screen_name=".$_GET['user'];
   }
-  else {
-    echo "err";
-  }
+  echo file_get_contents($url);
